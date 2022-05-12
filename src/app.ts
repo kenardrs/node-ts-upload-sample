9a9debc05eb8abc202bae4da43e5599d21dc29d2
kenard rodrigues da silva <kenardrs@gmail.com>
2022-05-12 18:18:39 -0300

    include parms test

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express'
 import multer from 'multer'
-// import os from 'node:os'
 // import cors from 'cors'
 
 class App {
   public express: express.Application
-  //   private upload: multer.Multer
+  private fileTypes: string[]
 
   public constructor () {
+    this.fileTypes = [
+      'image/gif',
+      'image/png',
+      'image/jpeg',
+      'application/pdf'
+    ]
     this.express = express()
     this.middleware()
     this.routes()
@@ -19,24 +24,42 @@ class App {
   }
 
   private routes (): void {
-    const upload = multer({ })
+    const upload = multer({})
 
     this.express.get('/', (req, res) => {
       return res.send('Hello Word')
     })
 
-    this.express.post('/upload', upload.single('file'), (req, res) => {
-      const title = req.body.title
-      const file = req.file
-      const encoded = req.file.buffer.toString('base64')
-      console.log('title', title)
-      console.log('file', file)
-      console.log('encoded{', encoded, '}')
-      //   console.log(file.filename)
-      return res.sendStatus(200)
+    this.express.post(
+      '/upload',
+      upload.single('file'),
+      (req: express.Request, res: express.Response) => {
+        const { file, body } = req
 
-    //   return res.sendStatus(200).send({ Arquivo: file.filename, tamanho: file.size, mensagem: 'enviado com sucesso' })
-    })
+        if (Object.keys(body).length === 0) {
+          return res
+            .status(401)
+            .send({ erro: 'O formulário de dados está vazio' })
+        }
+
+        if ((file.size > 1024 * 1024 * 4)) {
+          return res.status(401).send({
+            erro: 'O arquivo de imagem enviado é maior do que o permitido, max: 4MB'
+          })
+        }
+
+        if (!this.fileTypes.includes(file.mimetype)) {
+          return res.status(401).send({
+            erro: 'O tipo de arquivo de imagem enviado não é permitido'
+          })
+        }
+
+        console.log({ file })
+        return res
+          .status(200)
+          .send({ body, message: `${file.originalname} enviada com sucesso` })
+      }
+    )
   }
 }
 
